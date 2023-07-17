3ef072a9c4be136755a4959dec4d6434ebbf9d61
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-07-17 12:21:00 +0000

    commit

--- macros/update_dq_flag_tbl.sql
+++ macros/update_dq_flag_tbl.sql
@@ -1,10 +1,10 @@
 {% macro update_dq_flag_tbl(src_model, tgt_model, id) %}
     update {{ tgt_model }}
         set dq_flag = 'FALSE'
-    where {{ id }} in (select distinct {{ id }} from {{ src_model }}) ;
+    where {{ id }} in (select distinct {{ id }} from {{ src_model }}) and dq_flag = 'TRUE' ;
     update {{ tgt_model }}
         set dq_flag = 'TRUE'
-    where {{ id }} not in (select distinct {{ id }} from {{ src_model }}) ;
+    where {{ id }} not in (select distinct {{ id }} from {{ src_model }}) and dq_flag IS NULL;
 
     drop view if exists {{ src_model }};
 {% endmacro %}
