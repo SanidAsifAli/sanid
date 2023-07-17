89ed978548034c28c883d02883126cb0404559d9
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-07-17 09:05:29 +0000

    commit

--- models/stage/stg_src1_billing_history_tbl.sql
+++ models/stage/stg_src1_billing_history_tbl.sql
@@ -3,5 +3,5 @@ with bill_hist_tbl as (select * from {{ source("analytics", "billing_history_tbl
 select
     {{ dbt_utils.surrogate_key("KEY_HASH", "ROW_HASH") }} as billing_history_tbl_id,
     stg_src1_bill_hist_tbl_pk.nextval as stg_src1_billing_history_tbl_sk,
-    *
+    *,NULL as dq_check
 from bill_hist_tbl
