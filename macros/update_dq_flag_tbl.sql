43d38d463071978dc164d88e5f1932c53ddd65e0
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-07-17 12:52:26 +0000

    commit

--- macros/update_dq_flag_tbl.sql
+++ macros/update_dq_flag_tbl.sql
@@ -1,9 +1,9 @@
 {% macro update_dq_flag_tbl(src_model, tgt_model, id) %}
     update {{ tgt_model }}
         set dq_flag = 'FALSE',
-        dq_check = case when substring({{ src_model }},1,9)='not_nulls'
+        dq_check = case when substring('{{ src_model }}',1,9)='not_nulls'
 then 'NULL_CHECK' 
-when substring({{ src_model }},1,9)='unique_dq'
+when substring('{{ src_model }}',1,9)='unique_dq'
 then 'UNIQUE_CHECK' end
     where {{ id }} in (select distinct {{ id }} from {{ src_model }});
     update {{ tgt_model }}
