9d5e2c85cdf2d30ab05ae6207418bca83dc42514
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-07-24 10:47:16 +0000

    commit

--- models/stage/raw_valuation2.sql
+++ models/stage/raw_valuation2.sql
@@ -2,7 +2,7 @@ with raw_valuation2 as (select * from {{ source("dev_raw", "raw_valuation2") }})
 
 select
     {{ dbt_utils.surrogate_key("KEY_HASH", "ROW_HASH") }} as billing_history_tbl_id,
-    stg_src1_bill_hist_tbl_pk.nextval as stg_src1_billing_history_tbl_sk,
+    SEQ_CUR_VALUATION_2_PK.nextval as stg_src1_billing_history_tbl_sk,
     *,NULL as dq_check
 from raw_valuation2
 
