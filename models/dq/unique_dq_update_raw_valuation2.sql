9b710f535e9530cbc76727079b2807aa744299b5
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-07-24 13:11:31 +0000

    commit

--- models/dq/unique_dq_update_raw_valuation2.sql
+++ models/dq/unique_dq_update_raw_valuation2.sql
@@ -1,11 +1,11 @@
 select *
 from
-    {{ source("dq", "unique_dq_check_raw_valuation2") }}
+    {{ source("dq", "unique_check_raw_valuation2") }}
 
     {{
         config(
             post_hook=update_dq_flag_tbl(
-                "unique_dq_update_raw_valuation2", "cur_valuation2", "billing_history_tbl_id"
+                "unique_check_raw_valuation2", "cur_valuation2", "billing_history_tbl_id"
             )
         )
     }}
