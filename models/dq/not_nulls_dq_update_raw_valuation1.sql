1eb931f590d634d3b60a1506500174c55521e71e
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-07-25 07:05:46 +0000

    commit

--- models/dq/not_nulls_dq_update_raw_valuation1.sql
+++ models/dq/not_nulls_dq_update_raw_valuation1.sql
@@ -1,11 +1,11 @@
 select *
 from
-    {{ source("dq", "not_nulls_check_raw_valuation1") }}
+    {{ source("dq", "not_nulls_dq_update_raw_valuation1") }}
 
     {{
         config(
             post_hook=update_dq_flag_tbl(
-                "not_nulls_check_raw_valuation1", "cur_valuation1", "billing_history_id"
+                "not_nulls_dq_update_raw_valuation1", "cur_valuation1", "billing_history_id"
             )
         )
     }}
