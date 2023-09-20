aec7d5ac53b3066016b7e802caff850021fd1053
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-09-20 15:14:49 +0000

    commit

--- macros/call_meta_step_ctrl_end_sp.sql
+++ macros/call_meta_step_ctrl_end_sp.sql
@@ -1,8 +1,8 @@
-{% macro call_meta_step_ctrl_end_sp(step_sk) %}
+{% macro call_meta_step_ctrl_end_sp(step_sk,status) %}
 
     {% set query -%}
         begin transaction;
-        call eyiip_rga_db.meta.sp_step_end({{ step_sk }});
+        call eyiip_rga_db.meta.sp_step_end({{ step_sk }},{{status}});
         commit;
     {%- endset %}
 
--- tests/not_nulls_check_raw_valuation2.sql
+++ tests/not_nulls_check_raw_valuation2.sql
@@ -1,3 +1,3 @@
-{% set cols = not_nulls_cols_meta_rule_tbl("cur_valuation1") %}
+{% set cols = not_nulls_cols_meta_rule_tbl("cur_valuation2") %}
 
 {{ not_nulls_in_columns(ref("cur_valuation2"), cols) }}
