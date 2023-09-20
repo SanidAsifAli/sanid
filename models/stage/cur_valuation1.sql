2d5ec1cdee4937c5e355b9f03b14eab09bb6e616
sanid.asif.ali@gmail.com <sanid.asif.ali@gmail.com>
2023-09-20 14:14:21 +0000

    commit

--- models/stage/cur_valuation1.sql
+++ models/stage/cur_valuation1.sql
@@ -3,7 +3,7 @@ with raw_valuation1 as (select * from {{ source("dev_raw", "raw_valuation1") }})
 select
     {{ dbt_utils.surrogate_key("KEY_HASH", "ROW_HASH") }} as billing_history_id,
     EYIIP_RGA_DB.RAW_LOB1.SEQ_CUR_VALUATION_1_PK.nextval as stg_src1_billing_history_sk,
-    *,NULL as dq_check
+    *,NULL as dq_flag,NULL as dq_check
 from raw_valuation1
 
 {{ 
